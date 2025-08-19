9adbae799e82222e9b46185e21ed585225eee79f
hyeontae.lee <hyeontae.lee@inhuman-z.com>
2025-08-19 10:12:00 +0900

    feat: update import source path because file name change

--- apps/k6-runner-v2/src/routes/route.ts
+++ apps/k6-runner-v2/src/routes/route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { container } from '../container';
+import { container } from '../container/container';
 import { validateTestRequest } from '../middleware/validator';
 
 const router = Router();
